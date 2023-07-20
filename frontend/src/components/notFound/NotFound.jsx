e0d7c745f246c8c35d108877f70c8f4fbddd10d6
Ana <e4a.perez24@gmail.com>
2023-07-20 14:40:12 +0200

    notfound

--- frontend/src/components/notFound/NotFound.jsx
+++
@@ -1,9 +0,0 @@
-const NotFound = () => {
-    return (
-        <main className="not-found">
-            <h2>Página no encontrada - 404</h2>
-        </main>
-    );
-};
-
-export default NotFound;
--- frontend/src/pages/notFoundPage/NotFoundPage.jsx
+++ frontend/src/pages/notFoundPage/NotFoundPage.jsx
@@ -1,8 +1,20 @@
+import './notfoundpage.css';
+
 function NotFoundPage() {
   return (
-    <main className='notFound'>
-      <h2>Página no encontrada - 404</h2>
-    </main>
+    <div>
+      <div id='oopss'>
+        <div id='errorText'>
+          <img
+            src='https://cdn.rawgit.com/ahmedhosna95/upload/1731955f/sad404.svg'
+            alt={404}
+          />
+          <span>Error 404</span>
+          <p className='pa'>Page not found! ...</p>
+          <p className='pb'>You will be redirected ...</p>
+        </div>
+      </div>
+    </div>
   );
 }
 
