b998e532c57c7377bf9aec9b930a9ad44f03e6b3
Ana <e4a.perez24@gmail.com>
2023-07-24 15:10:16 +0200

    cambiocss

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import NavBar from '../shared/navBar/NavBar';
-
+import loho from '../../assets/loho.png';
 import { Link } from 'react-router-dom';
 import useAuth from '../../hooks/useAuth';
 import './header.css';
@@ -7,16 +7,18 @@ function Header() {
   const { logout, user } = useAuth();
   return (
     <>
-      <header className='header'>
-        <h1>
+      <header className='headerr'>
+        <h1 className='entryh1'>
           <Link className='linkStyle' to='/'>
             Ciudad Accesible
           </Link>
         </h1>
+
         <nav className='navb'>
-          {user && <p className='nameUser'>Hola @{user.username}!</p>}
+          {user && <p className='nameUser'>Hi @{user.username}!</p>}
         </nav>
       </header>
+
       <NavBar />
     </>
   );
