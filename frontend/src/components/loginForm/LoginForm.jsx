ef2d8600153317cc496705befd2590a2c33920a4
Ana <e4a.perez24@gmail.com>
2023-07-27 11:13:59 +0200

    mas css

--- frontend/src/components/loginForm/LoginForm.jsx
+++ frontend/src/components/loginForm/LoginForm.jsx
@@ -32,17 +32,27 @@ function LoginForm({ login }) {
   return (
     <div className='login-container'>
       <div className='image-container'>
-        <img className='imagenCiudad' src='https://tipsparatuviaje.com/wp-content/uploads/2019/11/rotterdam-ho.jpg' />
-        <Link className='logoWeb' to="/">
-            <img className='logoGoHome' src='../src/assets/C-A_white.png' alt='logo web' />
+        <img
+          className='imagenCiudad'
+          src='https://images.pexels.com/photos/380283/pexels-photo-380283.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+        />
+        <Link className='logoWeb' to='/'>
+          <img
+            className='logoGoHome'
+            src='../src/assets/C-A.svg'
+            alt='logo web'
+          />
         </Link>
-        <h1 className='h1LoginRegister' >Ciudad Accesible</h1>
+        <h1 className='h1LoginRegister'>Ciudad Accesible</h1>
       </div>
 
       <form className='formLogin' onSubmit={handleSubmit}>
         <h2 className='h2Login'>Login</h2>
-        <label className='labelForm' htmlFor='email'>Email: </label>
-        <input className='inputLogin'
+        <label className='labelForm' htmlFor='email'>
+          Email:{' '}
+        </label>
+        <input
+          className='inputLogin'
           type='email'
           id='email'
           value={email}
@@ -51,8 +61,11 @@ function LoginForm({ login }) {
           maxLength='100'
           required
         />
-        <label className='labelForm' htmlFor='password'>Password:</label>
-        <input className='inputLogin'
+        <label className='labelForm' htmlFor='password'>
+          Password:
+        </label>
+        <input
+          className='inputLogin'
           type='password'
           id='password'
           value={password}
@@ -61,7 +74,7 @@ function LoginForm({ login }) {
           maxLength='100'
           required
         />
-        <button className='buttonLogin' >Loguearse</button>
+        <button className='buttonLogin'>Loguearse</button>
         {loading && <Spinner />}
         {errMess && <ErrorMessage msg={errMess} />}
       </form>
