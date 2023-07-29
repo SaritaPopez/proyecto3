bbd85475aa0e913af719e49b2088285dddcb557e
Javier Romero Garcia <125921160+jaroga19@users.noreply.github.com>
2023-07-29 10:10:28 +0200

    Update Footer.jsx
    Cambiodel nombre de los logotipos, añadidos a assets previamente:
    - plus.png
    - casa.png
    - cerrar-sesion.png

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -11,17 +11,17 @@ function Footer() {
       <>
       <div className='container-entries'>
         <Link to='/message'>
-        <img src='../src/assets/mas2.png' alt='icono mas' />
+        <img src='../src/assets/plus.png' alt='icono mas' />
         </Link>
       </div>
       <div className='container-home'>
         <Link to="/">
-            <img src='../src/assets/home2.png' alt='icono casa' />
+            <img src='../src/assets/casa.png' alt='icono casa' />
         </Link>
       </div>
       <div className='container-logout'>
       <Link className='button-logout' onClick={logout}>
-          <img src='../src/assets/logout2.png' alt='icono logout' />
+          <img src='../src/assets/cerrar-sesion.png' alt='icono logout' />
         </Link>
       </div>
       </>
