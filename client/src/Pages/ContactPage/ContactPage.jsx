233833a82c5542016334d9f2cf87c3c78f65bcf2
Ana <e4a.perez24@gmail.com>
2023-08-04 15:43:06 +0200

    logo contact

--- client/src/Pages/ContactPage/ContactPage.jsx
+++ client/src/Pages/ContactPage/ContactPage.jsx
@@ -6,43 +6,47 @@ import './contactpage.css';
 
 function ContactPage() {
   return (
-    <div className='fondop'>
+    <>
       <Link className='linkStyle' to='/'>
-        <img
-          className='imgHeaderContact'
-          src='../src/Assets/C-A.svg'
-          alt='logo web'
-        />
-      </Link>
-      <section className='containerContact'>
-        <h2 className='titleContact'>
-          <span className='primary'>Meet our team</span>
-        </h2>
-        <div className='gallery-wrapper'>
-          <figure className='gallery-item'>
-            <img src={Ana} alt='mi foto' className='item-image' />
-            <figcaption className='item-description'>
-              <h2 className='name'>Ana Pérez</h2>
-              <span className='role'> Web Developer Junior</span>
-            </figcaption>
-          </figure>
-          <figure className='gallery-item'>
-            <img src={Sara} alt='mi foto' className='item-image' />
-            <figcaption className='item-description'>
-              <h2 className='name'>Sara López</h2>
-              <span className='role'> Web Developer Junior</span>
-            </figcaption>
-          </figure>
-          <figure className='gallery-item'>
-            <img src={javi} alt='mi foto' className='item-image' />
-            <figcaption className='item-description'>
-              <h2 className='name'>Javier Romero</h2>
-              <span className='role'> Web Developer Junior</span>
-            </figcaption>
-          </figure>
+        <div className='logoContact'>
+          <img
+            className='imgHeaderContact'
+            src='../src/Assets/C-A.svg'
+            alt='logo web'
+          />
         </div>
-      </section>
-    </div>
+      </Link>
+      <div className='fondop'>
+        <section className='containerContact'>
+          <h2 className='titleContact'>
+            <span className='primary'>Meet our team</span>
+          </h2>
+          <div className='gallery-wrapper'>
+            <figure className='gallery-item'>
+              <img src={Ana} alt='mi foto' className='item-image' />
+              <figcaption className='item-description'>
+                <h2 className='name'>Ana Pérez</h2>
+                <span className='role'> Junior Web Developer </span>
+              </figcaption>
+            </figure>
+            <figure className='gallery-item'>
+              <img src={Sara} alt='mi foto' className='item-image' />
+              <figcaption className='item-description'>
+                <h2 className='name'>Sara López</h2>
+                <span className='role'> Web Developer Junior</span>
+              </figcaption>
+            </figure>
+            <figure className='gallery-item'>
+              <img src={javi} alt='mi foto' className='item-image' />
+              <figcaption className='item-description'>
+                <h2 className='name'>Javier Romero</h2>
+                <span className='role'> Web Developer Junior</span>
+              </figcaption>
+            </figure>
+          </div>
+        </section>
+      </div>
+    </>
   );
 }
 
