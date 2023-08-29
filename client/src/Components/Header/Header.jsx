e0e8f8d33f1327839d5ced1cdd54c9964c1fd872
usuario <e4a.perez24@gmail.com>
2023-08-29 17:23:31 +0200

    icono telefono cambiado

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -1,29 +1,29 @@
-import NavBar from '../Shared/navBar/NavBar';
-import { BsTelephoneInbound } from 'react-icons/bs';
+import NavBar from "../Shared/navBar/NavBar";
+import { BsFillPersonFill } from "react-icons/bs";
 
-import { Link } from 'react-router-dom';
-import useAuth from '../../Hooks/useAuth';
-import './header.css';
+import { Link } from "react-router-dom";
+import useAuth from "../../Hooks/useAuth";
+import "./header.css";
 function Header() {
   const { logout, user } = useAuth();
   return (
     <>
-      <header className='headerr'>
-        <div className='header-container'>
-          <Link className='contacts' to='/contact'>
-            <BsTelephoneInbound />
+      <header className="headerr">
+        <div className="header-container">
+          <Link className="contacts" to="/contact">
+            <BsFillPersonFill />
           </Link>
-          <h1 className='h1Header'>Ciudad Accesible</h1>
+          <h1 className="h1Header">Ciudad Accesible</h1>
         </div>
-        <Link className='linkStyle' to='/'>
+        <Link className="linkStyle" to="/">
           <img
-            className='imgHeader'
-            src='../src/Assets/C-A.svg'
-            alt='logo web'
+            className="imgHeader"
+            src="../src/Assets/C-A.svg"
+            alt="logo web"
           />
         </Link>
-        <nav className='navb'>
-          {user && <p className='nameUser'>Hi @{user.username}!</p>}
+        <nav className="navb">
+          {user && <p className="nameUser">Hi @{user.username}!</p>}
         </nav>
       </header>
 
